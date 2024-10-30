c569167f5c2cb05cd4aac46a98481d0095f36dcb
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-10-30 11:45:49 +0300

    Added google and github images to be allowed by next and argon webpack for password hashing

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      staleTimes: {
-        dynamic: 30,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
       },
-    },
-    serverExternalPackages: ["@node-rs/argon2"],
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "utfs.io",
-          pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
-        },
-      ],
-    },
-    rewrites: () => {
-      return [
-        {
-          source: "/hashtag/:tag",
-          destination: "/search?q=%23:tag",
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  rewrites: () => {
+    return [
+      {
+        source: "/hashtag/:tag",
+        destination: "/search?q=%23:tag",
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@node-rs/argon2");
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
