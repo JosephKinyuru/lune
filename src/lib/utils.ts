db83b4513c13d4ea6f5288dd27e38b39a064fcb2
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-10-30 11:50:27 +0300

    modified utility folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { formatDate, formatDistanceToNowStrict } from "date-fns";
 import { twMerge } from "tailwind-merge";
+import prisma from "@/lib/prisma";
+import crypto from "crypto";
+import { randomBytes } from "crypto";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -33,3 +36,58 @@ export function slugify(input: string): string {
     .replace(/[^a-z0-9-]/g, "");
 }
 
+export async function generateRandomToken(length: number) {
+  const buf = await new Promise<Buffer>((resolve, reject) => {
+    crypto.randomBytes(Math.ceil(length / 2), (err, buf) => {
+      if (err !== null) {
+        reject(err);
+      } else {
+        resolve(buf);
+      }
+    });
+  });
+
+  return buf.toString("hex").slice(0, length);
+}
+
+export const generateOTP = (length: number): string => {
+  const otp = Array.from({ length }, () => Math.floor(Math.random() * 10)).join(
+    "",
+  );
+  return otp;
+};
+
+export async function createTransaction<T extends typeof prisma>(
+  cb: (trx: T) => Promise<void>,
+) {
+  // Prisma's transaction API using the `prisma` client
+  await prisma.$transaction(async (trx) => {
+    await cb(trx as T);
+  });
+}
+
+export async function generateUniqueUsername(fullName: string) {
+  const baseUsername = fullName.toLowerCase().replace(/\s+/g, ""); 
+
+  const appendRandom = (username: string) => {
+    const randomString = randomBytes(2).toString("hex"); 
+    return `${username}${randomString}`; 
+  };
+
+  let username = baseUsername;
+  let isUnique = false;
+
+  while (!isUnique) {
+    const existingUser = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!existingUser) {
+      isUnique = true;
+    } else {
+      username = appendRandom(baseUsername); 
+    }
+  }
+
+  return username;
+}
