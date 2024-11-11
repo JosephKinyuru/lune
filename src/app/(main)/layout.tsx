ea9cadef33bfc42235e168ed49c96ff932a09386
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-11-11 15:34:28 +0300

    Changed layout of main page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,10 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import MenuBar from "./MenuBar";
-import Navbar from "./Navbar";
 import SessionProvider from "./SessionProvider";
 import LeftSidebar from "./LeftSidebar";
-import RightSidebar from "./RightSidebar";
+import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 
 export default async function Layout({
   children,
@@ -15,20 +15,32 @@ export default async function Layout({
 
   if (!session.user) redirect("/sign-in");
 
+  const [unreadNotificationsCount, unreadMessagesCount] = await Promise.all([
+    prisma.notification.count({
+      where: {
+        recipientId: session.user.id,
+        read: false,
+      },
+    }),
+    (await streamServerClient.getUnreadCount(session.user.id))
+      .total_unread_count,
+  ]);
+
   return (
     <SessionProvider value={session}>
-      <div className="">
-        <Navbar />
+      <div className="min-h-screen overflow-clip bg-card dark:bg-black">
+        <div className="flex justify-center md:mx-14 lg:mx-12 xl:mx-8 2xl:mx-24">
+          <LeftSidebar
+            unreadNotificationsCount={{
+              unreadCount: unreadNotificationsCount,
+            }}
+            unreadMessagesCount={{ unreadCount: unreadMessagesCount }}
+          />
 
-        <div className="flex flex-grow">
-          <LeftSidebar />
-          <section className="flex min-h-screen flex-1 flex-col items-center px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
-            <div className="w-full max-w-4xl">{children}</div>
-          </section>
-          <RightSidebar />
+          {children}
         </div>
-        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-4 border-t bg-card p-3 md:hidden" />
       </div>
+      <MenuBar className="sticky bottom-0 flex w-full justify-center gap-4 border-t bg-card p-3 md:hidden" />
     </SessionProvider>
   );
 }
